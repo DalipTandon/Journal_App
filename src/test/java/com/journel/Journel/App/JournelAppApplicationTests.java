05e65ebe3882db978e2fcf5c29aa526b84542ebc
DalipTandon <daliptandon8@gmail.com>
2026-05-16 16:24:07 +0530

    init

---
+++ src/test/java/com/journel/Journel/App/JournelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journel.Journel.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
